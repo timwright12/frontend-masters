ebb6964f456977a6055b7dcea78d8c43a0b4b788
Tim Wright <timwright12@gmail.com>
2022-07-06 09:41:38 -0400

    Added electron prototype markdown files

---
+++ electron/firesale/app/main.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const { app, BrowserWindow, dialog } = require('electron');
+
+// Keeps mainWindow from getting garbage-collected
+let mainWindow;
+
+app.on('ready', () => {
+
+  // Create browswer instance, but don't show it
+  mainWindow = new BrowserWindow({ show: false });
+
+  // Load HTML file
+  mainWindow.loadFile(`${__dirname}/index.html`);
+
+  // Show the window after the file is loaded to avoid a white screen
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+});
+
+// Let mainProcess talk to renderer
+exports.getFileFromUser = () => {
+  const files = dialog.showOpenDialog({
+    properties: ['openFile'],
+    buttonLabel: 'Choose file / Open',
+    title: 'Open fire sale document',
+    filters: [
+      { name: 'Markdown Files', extensions: ['md', 'markdown', 'mdown'] },
+      { name: 'Text Files', extensions: ['txt', 'text'] },
+    ]
+  });
+  if(!files) return;
+
+  const file = files[0];
+  openFile(file);
+};
+
+const openFile = (file) => {
+  const content = fs.readFileSync(file).toString();
+
+  // Set "file-opened" event, this is a custom event name
+  mainWindow.webContents.send(
+    'file-opened',
+    file,
+    content
+  );
+};
---
+++ electron/firesale/app/renderer.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const marked = require('marked');
+
+const { remote, ipcRenderer } = require('electron');
+
+let filePath = null;
+let originalContent = '';
+
+const mainProcess = remote.require('./main.js');
+const currentWindow = remote.getCurrentWindow();
+
+const markdownView = document.querySelector('#markdown');
+const htmlView = document.querySelector('#html');
+const newFileButton = document.querySelector('#new-file');
+const openFileButton = document.querySelector('#open-file');
+const saveMarkdownButton = document.querySelector('#save-markdown');
+const revertButton = document.querySelector('#revert');
+const saveHtmlButton = document.querySelector('#save-html');
+const showFileButton = document.querySelector('#show-file');
+const openInDefaultButton = document.querySelector('#open-in-default');
+
+const renderMarkdownToHtml = markdown => {
+  htmlView.innerHTML = marked(markdown, { sanitize: true });
+};
+
+const updateUserInterface = (isEdited) => {
+  let title = 'Fire Sale';
+  if (filePath) {
+    title = `${path.basename(filePath)} - ${title}`;
+  }
+  if (isEdited) {
+    title = `${title} (Edited)`;
+  }
+
+  saveMarkdownButton.disabled = !isEdited;
+  revertButton.disabled = !isEdited;
+
+  currentWindow.setTitle(title);
+
+  currentWindow.setRepresentedFilename(filePath);
+  currentWindow.setDocumentEdited(isEdited);
+
+};
+
+markdownView.addEventListener('keyup', event => {
+  const currentContent = event.target.value;
+
+  renderMarkdownToHtml(currentContent);
+  updateUserInterface(currentContent !== originalContent);
+});
+
+openFileButton.addEventListener('click', () => {
+  mainProcess.getFileFromUser();
+});
+
+newFileButton.addEventListener('click', () => {
+  alert('Create new file!');
+});
+
+saveMarkdownButton.addEventListener('click', () => {
+  alert('Save Markdown');
+});
+
+revertButton.addEventListener('click', () => {
+  markdownView.value = originalContent;
+  renderMarkdownToHtml(originalContent);
+  updateUserInterface();
+});
+
+// Event listener from main.js
+ipcRenderer.on('file-opened', (event, file, content) => {
+
+  filePath = file;
+  originalContent = content;
+
+  markdownView.value = content;
+  renderMarkdownToHtml(content);
+  updateUserInterface();
+});
